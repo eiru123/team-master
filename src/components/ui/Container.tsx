6a9f4e96b025e2686c8c346d318c464986f55908
SeungLee <ls0408@naver.com>
2024-10-29 22:35:59 +0900

    update: 기본화면 추가

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 
-interface ContainerProps {
+interface ContainerProps extends React.ComponentProps<'div'> {
   children: React.ReactNode;
   display: 'flex' | 'block' | null;
 }
 
-const Container: React.FC<ContainerProps> = ({ children, display }) => {
+const Container: React.FC<ContainerProps> = ({
+  children,
+  display,
+  ...rest
+}) => {
   switch (display) {
     case 'flex':
       return (
-        <main className="bg-main dark:bg-darkMain w-full h-screen relative flex flex-col justify-center items-center">
+        <main
+          className={`bg-main dark:bg-darkMain w-full h-screen relative flex flex-col justify-center items-center ${rest.className}`}
+        >
           {children}
         </main>
       );
 
     case 'block':
       return (
-        <main className="bg-main dark:bg-darkMain w-full h-full min-h-screen relative">
+        <main
+          className={`bg-main dark:bg-darkMain w-full h-full min-h-screen relative ${rest.className}`}
+        >
           {children}
         </main>
       );
