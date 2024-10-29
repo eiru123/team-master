11b25290a6bbdd1b9208304d0bb3fbe021324267
SeungLee <ls0408@naver.com>
2024-10-29 22:34:53 +0900

    update: shadcn 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,19 +16,72 @@ const config: Config = {
         main: '#F5F7FF',
         darkMain: '#090A0D',
         infoCentre: '#E4DECB',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
       },
       keyframes: {
         goTop: {
-          '0%': { top: '50%' },
-          '100%': { top: '2rem' },
+          '0%': {
+            top: '50%',
+          },
+          '100%': {
+            top: '2rem',
+          },
         },
         fadeIn: {
-          from: { opacity: '0' },
-          to: { opacity: '1' },
+          from: {
+            opacity: '0',
+          },
+          to: {
+            opacity: '1',
+          },
         },
         fadeOut: {
-          from: { opacity: '1' },
-          to: { opacity: '0', display: 'none' },
+          from: {
+            opacity: '1',
+          },
+          to: {
+            opacity: '0',
+            display: 'none',
+          },
         },
       },
       animation: {
@@ -37,6 +91,11 @@ const config: Config = {
       },
       transformOrigin: {
         transform: 'translate(-50%, -50%)',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
@@ -51,6 +110,8 @@ const config: Config = {
       };
       addUtilities(newUtilities, ['responsive', 'hover']);
     },
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('tailwindcss-animate'),
   ],
 };
 export default config;
